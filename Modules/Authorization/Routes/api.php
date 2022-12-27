099a4742c3cd3269ddfb444998b20309b59755e5
mohsen <mohsen.abedy@yahoo.com>
2022-12-27 21:52:20 +0330

    fix APIs structure

--- Modules/Authorization/Routes/api.php
+++ Modules/Authorization/Routes/api.php
@@ -6,19 +6,23 @@ use Modules\Authorization\Http\Controllers\V1;
 Route::prefix("v1/admin")->name("v1.admin.")
     ->middleware(["auth:api", "role:admin,developer"])->group(function () {
 
-        Route::prefix("roles")->name("roles.")->group(function () {
-            Route::get("/", [V1\Roles\RoleController::class, "index"])->name("index");
-            Route::post("", [V1\Roles\RoleController::class, "store"])->name("store");
-            Route::get("{id}", [V1\Roles\RoleController::class, "show"])->name("show");
-            Route::put("{id}", [V1\Roles\RoleController::class, "update"])->name("update")->middleware("can:update role");
-            Route::delete("{id}", [V1\Roles\RoleController::class, "destroy"])->name("destroy")->middleware("can:delete role");
-        });
+        Route::controller(V1\Roles\RoleController::class)
+            ->prefix("roles")->name("roles.")->group(function () {
 
-        Route::prefix("permissions")->name("permissions.")->group(function () {
-            Route::get("/", [V1\Permissions\PermissionController::class, "index"])->name("index");
-            Route::post("", [V1\Permissions\PermissionController::class, "store"])->name("store");
-            Route::get("{id}", [V1\Permissions\PermissionController::class, "show"])->name("show");
-            Route::put("{id}", [V1\Permissions\PermissionController::class, "update"])->name("update")->middleware("can:update permission");
-            Route::delete("{id}", [V1\Permissions\PermissionController::class, "destroy"])->name("destroy")->middleware("can:delete permission");
-        });
+                Route::get("/", "index")->name("index");
+                Route::post("", "store")->name("store");
+                Route::get("{id}", "show")->name("show");
+                Route::put("{id}", "update")->name("update")->middleware("can:update role");
+                Route::delete("{id}", "destroy")->name("destroy")->middleware("can:delete role");
+            });
+
+        Route::controller(V1\Permissions\PermissionController::class)
+            ->prefix("permissions")->name("permissions.")->group(function () {
+
+                Route::get("/", "index")->name("index");
+                Route::post("", "store")->name("store");
+                Route::get("{id}", "show")->name("show");
+                Route::put("{id}", "update")->name("update")->middleware("can:update permission");
+                Route::delete("{id}", "destroy")->name("destroy")->middleware("can:delete permission");
+            });
     });
