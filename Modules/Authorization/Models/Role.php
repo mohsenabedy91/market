bcc1121cdf0243307ced8af32b2e7a2edcce0604
mohsen <mohsen.abedy@yahoo.com>
2022-12-28 10:01:23 +0330

    add softDelete trait in Role & Permission Model, fix fillable property

--- Modules/Authorization/Models/Role.php
+++ Modules/Authorization/Models/Role.php
@@ -4,17 +4,25 @@ namespace Modules\Authorization\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Modules\Authorization\Database\Factories\RoleFactory;
-use Modules\Authorization\Services\Permissions\Traits\HasPermissions;
+use Modules\Authorization\Services\V1\Permissions\Traits\HasPermissions;
 
 /**
+ * @property int $id
  * @property string $name
+ * @property int $created_by
+ * @property int $updated_by
  */
 class Role extends Model
 {
-    use HasFactory, HasPermissions;
+    use HasFactory, HasPermissions, SoftDeletes;
 
-    protected $fillable = [];
+    protected $fillable = [
+        "name",
+        "created_by",
+        "updated_by"
+    ];
 
     /**
      * @return RoleFactory
